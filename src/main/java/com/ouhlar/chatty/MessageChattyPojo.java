c80231c06bc2636791879bea03eeefe77ad7d53b
duchzjety <oliverkolombo@gmail.com>
2022-04-03 18:28:01 +0200

    configuration of storage and web sockets

---
+++ src/main/java/com/ouhlar/chatty/MessageChattyPojo.java
@@ -0,0 +1,30 @@
+package com.ouhlar.chatty;
+
+public class MessageChattyPojo {
+    private String message;
+    private String sender;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageChattyPojo{" +
+                "message='" + message + '\'' +
+                ", sender='" + sender + '\'' +
+                '}';
+    }
+}
